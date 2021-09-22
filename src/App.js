6528a640178c44f1106f5073a20bbbe1369c505b
Kevin-Solomon <80545228+Kevin-Solomon@users.noreply.github.com>
2021-09-22 10:45:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import { useState } from "react";
 import "./styles.css";
 const movies = [
-  { name: "Tenet", rating: "5/5", genre: "action" },
-  { name: "Extraction", rating: "5/5", genre: "action" },
-  { name: "Anna", rating: "3.5/5", genre: "action" },
-  { name: "Jumaji", rating: "4/5", genre: "comedy" },
-  { name: "Tag", rating: "3.9/5", genre: "comedy" },
-  { name: "Work It", rating: "4.5/5", genre: "comedy" },
-  { name: "Shining", rating: "5/5", genre: "horror" },
-  { name: "Annabelle", rating: "4/5", genre: "horror" },
-  { name: "Oxygen", rating: "4.5/5", genre: "horror" }
+  {
+    name: "Tenet",
+    description:
+      "The film follows a secret agent who learns to manipulate the flow of time to prevent an attack from the future that threatens to annihilate the present world.",
+    rating: "5/5",
+    genre: "action"
+  },
+  {
+    name: "Extraction",
+    description:
+      "The film is about an Australian black ops mercenary whose mission to rescue an Indian drug lords kidnapped son in Dhaka",
+    rating: "5/5",
+    genre: "action"
+  },
+  {
+    name: "Anna",
+    description:
+      "a young Russian beauty who has been the victim of domestic abuse, will do anything to escape the life she is trapped in",
+    rating: "3.5/5",
+    genre: "action"
+  },
+  {
+    name: "Jumaji",
+    description:
+      "The story focuses on a group of teenagers who come across Jumanji—now transformed into a video game—twenty-one years after the events of the 1995 film.",
+    rating: "4/5",
+    genre: "comedy"
+  },
+  {
+    name: "Tag",
+    description:
+      "The film is based on a true story that was published in The Wall Street Journal about a group of grown men",
+    rating: "3.9/5",
+    genre: "comedy"
+  },
+  {
+    name: "Work It",
+    description:
+      'Quinn discovers a former champion of the "Work It" competition, Jake Taylor, who stopped competing after a knee injury two years prior. Quinn approaches him at the dance studio where he now works and asks him to be a choreographer for her team.',
+    rating: "4.5/5",
+    genre: "comedy"
+  },
+  {
+    name: "Shining",
+    description:
+      "The Shining is set in Colorado in the 1970s. It centres on the Torrance family: husband Jack, wife Wendy, and their five-year-old son, Danny. ",
+    rating: "5/5",
+    genre: "horror"
+  },
+  {
+    name: "Annabelle",
+    description:
+      "The film was inspired by a story of a doll named Annabelle told by Ed and Lorraine Warren.",
+    rating: "4/5",
+    genre: "horror"
+  },
+  {
+    name: "Oxygen",
+    description:
+      "A woman awakens in an airtight medical cryogenic unit, and discovers that she is trapped and that the units oxygen levels are falling rapidly. ",
+    rating: "4.5/5",
+    genre: "horror"
+  }
 ];
 export default function App() {
   const [movie, setMovies] = useState(movies);
@@ -71,6 +125,7 @@ export default function App() {
           >
             {mov.name}
             <div>
+              <div>{mov.description}</div>
               <small
                 style={{
                   display: "block",
